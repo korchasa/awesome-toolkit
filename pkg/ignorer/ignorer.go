6b8d31ae68a64171c3078290a3c038805457adc0
korchasa <korchasa@icloud.com>
2023-05-01 02:43:15 +0300

    fix: ignore special chars in ignorer, fix limitˆ

--- pkg/ignorer/ignorer.go
+++ pkg/ignorer/ignorer.go
@@ -1,42 +1,51 @@
 package ignorer
 
 import (
-    "github.com/korchasa/awesome-toolkit/pkg/list"
-    "unicode"
+	"github.com/korchasa/awesome-toolkit/pkg/list"
+	log "github.com/sirupsen/logrus"
+	"strings"
+	"unicode"
 )
 
-const maxNonLatinSymbolsPercent = 0.1
+const maxNonLatinSymbolsPercent = 3
 
 type Ignorer struct {
 }
 
 func NewIgnorer() *Ignorer {
-    return &Ignorer{}
+	return &Ignorer{}
 }
 
 func (i *Ignorer) ResolveIgnores(item *list.Item, readme string) {
-    if nonLatinPercentage(item.Description+readme) > maxNonLatinSymbolsPercent {
-        item.Ignore = true
-        item.IgnoreReason = "non latin description"
-    }
+	text := item.Description + readme
+	skipSyms := []string{"`", "*", "_", "{", "}", "[", "]", "(", ")", "#", "+", "-", ".", "!", "|"}
+	for _, sym := range skipSyms {
+		text = strings.ReplaceAll(text, sym, " ")
+	}
+	p := nonLatinPercentage(text)
+	if p > maxNonLatinSymbolsPercent {
+		log.Infof("non latin description of %s (%f)", item.Link, p)
+		item.Ignore = true
+		item.IgnoreReason = "non latin description"
+	}
 }
 
 func nonLatinPercentage(s string) float64 {
-    if len(s) == 0 {
-        return 0
-    }
-    nonLatinCount := 0
-    totalChars := 0
-    for _, r := range s {
-        if unicode.IsLetter(r) {
-            totalChars++
-            if !unicode.In(r, unicode.Latin) {
-                nonLatinCount++
-            }
-        }
-    }
-    if totalChars == 0 {
-        return 0
-    }
-    return float64(nonLatinCount) / float64(totalChars) * 100
+	if len(s) == 0 {
+		return 0
+	}
+	nonLatinCount := 0
+	totalChars := 0
+	for _, r := range s {
+		if unicode.IsLetter(r) {
+			totalChars++
+			if !unicode.In(r, unicode.Latin) {
+				nonLatinCount++
+			}
+		}
+	}
+	if totalChars == 0 {
+		return 0
+	}
+	return float64(nonLatinCount) / float64(totalChars) * 100
 }
